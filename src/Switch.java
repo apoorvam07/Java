1dfe47cea457931c144019729cf2a9a3683fbbad
apoorvam07 <apoorvakshtriya18@gmail.com>
2023-11-18 00:49:23 +0530

    switch

--- src/Switch.java
+++ src/Switch.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Switch {
+import java.util.Scanner;
+
+public class Switch {
+        public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+
+//        String userInput= input.nextLine();
+//        switch(userInput) {
+//            case "1":
+//                System.out.printf("%d%n", a);
+//                break;
+//            case "2":
+//                System.out.printf("%d%n", a + 1);
+//                break;
+//            case "3":
+//                System.out.printf("%d%n", a + 2);
+//                break;
+//            default:
+//                System.out.printf("%d%n", 100);
+//                break;
+            String flower = input.next();
+            switch (flower) {
+                case "lotus":
+                    System.out.println("wow");
+                    break;
+                case "lilly":
+                    System.out.println("beauty");
+                    break;
+                case "rose":
+                    System.out.println("cool");
+                    break;
+                default:
+                    System.out.println("all are beautiful");
+
+
+
+        }}
+
 }
--- src/ifelse.java
+++ src/ifelse.java
@@ -14,21 +14,6 @@ public class ifelse {
         } else if (b < 1) {
             System.out.printf("%d%n", a-1);
         }
-//        String userInput= input.nextLine();
-//        switch(userInput) {
-//            case "1":
-//                System.out.printf("%d%n", a);
-//                break;
-//            case "2":
-//                System.out.printf("%d%n", a + 1);
-//                break;
-//            case "3":
-//                System.out.printf("%d%n", a + 2);
-//                break;
-//            default:
-//                System.out.printf("%d%n", 100);
-//                break;
-
-
-//        }
-    }}
+//
+    }
+}
