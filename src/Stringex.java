1633653d0e637f700c683788e0ebedde4e6adcb5
apoorvam07 <apoorvakshtriya18@gmail.com>
2023-11-19 12:09:34 +0530

    first commit

---
+++ src/Stringex.java
@@ -0,0 +1,12 @@
+public class Stringex {
+    public static void main(String[] args) {
+        String m=greet("apoo" );
+        System.out.println(m);
+    }
+    static String greet(String name){
+        String greetings="HI"+ name;
+        return greetings;
+
+
+    }
+}
---
+++ src/method.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class method {
+    public static void printJava(){
+        System.out.println("hi");
+    }
+    public static void printName(String name){
+        System.out.println(name);
+    }
+    public static void printSum(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void greet(String m) {
+        System.out.println(m);
+    }
+
+    public static void main(String[] args) {
+        printJava();
+        printName("appu");
+        printSum(1,6);
+        greet("m");
+
+    }
+}
